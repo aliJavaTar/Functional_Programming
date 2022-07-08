adc9eb6d8b4ea6bc993812755a805f9a34e6cc4d
alijavaboy <ali77erfagh99@gmail.com>
2022-07-08 13:10:20 +0430

    refactore test

--- src/test/java/functional/principal/FirstCitizenFunctionShould.java
+++ src/test/java/functional/principal/FirstCitizenFunctionShould.java
@@ -3,9 +3,7 @@ package functional.principal;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -17,12 +15,37 @@ public class FirstCitizenFunctionShould {
 
     @Test
     void be_pass_as_method_parameter() {
-        List<String> players = new LinkedList<>();
-        players.add("Ronaldo");
-        players.add("Carlos");
-        players.sort(String::compareTo);
+        List<String> players = getPlayers(new LinkedList<>());
+        players.sort(getCompareTo());
+        assrtComper(players);
+    }
+
+
+    @Test
+    void be_pass_as_value_toVariable() {
+        List<String> players = getStrings();
+        Comparator<String> comparator = getCompareTo();
+        players.sort(comparator);
+        assrtComper(players);
+    }
+
+    private Comparator<String> getCompareTo() {
+        return String::compareTo;
+    }
+
+    private List<String> getStrings() {
+        List<String> players = getPlayers(new LinkedList<>());
+        return players;
+    }
+
+    private void assrtComper(List<String> players) {
         assertThat(players.get(0)).isEqualTo("Carlos");
         assertThat(players.get(1)).isEqualTo("Ronaldo");
+    }
 
+    private List<String> getPlayers(LinkedList<String> players) {
+        players.add("Ronaldo");
+        players.add("Carlos");
+        return players;
     }
 }
